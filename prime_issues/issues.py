9c5e088fbbf77bc17d59a1b0e9a41b09b14e48b1
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-17 16:10:00 -0500

    Start the initialization of the git commit tracking

--- prime_issues/issues.py
+++ prime_issues/issues.py
@@ -1,7 +1,18 @@
 from argparse import Namespace
+from warnings import filterwarnings
 
 from prime_issues.args.issuesArgs import getArgs
+from prime_issues.utils.config import Config
+
+filterwarnings(action="ignore")
 
 
 def main() -> None:
     args: Namespace = getArgs()
+    config: Config = Config(args=args)
+
+    match args.tracker:
+        case "gh":
+            pass
+        case _:
+            exit(1)
